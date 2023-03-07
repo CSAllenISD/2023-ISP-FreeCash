7c44e89f4161de9cef08bb3897fd3ca1b074ac64
squaredcubed64 <111006607+squaredcubed64@users.noreply.github.com>
2023-03-07 05:58:25 +0000

    Added environment variables for keys and tokens

--- tweetscraper.py
+++ tweetscraper.py
@@ -1,19 +1,20 @@
-"""This file prints recent tweets with a certain stock ticker using Tweepy"""
+"""This file finds recent tweets with a certain stock ticker and writes their text and creation dates to a file"""
 import tweepy
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+#Get variables from environment (from .env)
+BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 
 #Number of Tweets to be pulled. Only 500,000 can be pulled per month.
 max_results = 10
-#Enter Twitter API keys
-BEARER_TOKEN = ''
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_TOKEN_SECRET = ''
 
-# You can authenticate as your app with just your bearer token
-
-# You can provide the consumer key and secret with the access token and access
-# token secret to authenticate as a user
+#The following are environment variables stored in env.local, which should be in .gitignore
 client = tweepy.Client(
     bearer_token=BEARER_TOKEN,
     consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET,
