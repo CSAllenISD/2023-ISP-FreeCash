c4ad345e624f2a5618e78f4d7da1d941ea16658d
squaredcubed64 <111006607+squaredcubed64@users.noreply.github.com>
2023-03-07 04:35:02 +0000

    Pulled Tweets and dates now go to tweets.txt

--- tweetscraper.py
+++ tweetscraper.py
@@ -4,12 +4,11 @@ import tweepy
 #Number of Tweets to be pulled. Only 500,000 can be pulled per month.
 max_results = 10
 #Enter Twitter API keys
-BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAACK0lgEAAAAAwbhuEpAJPc%2FzvtA4GfgDVAQ0pM4%3'\
-    'Dn8noDHkYdSq68Gb5w6K9vSHFIYPWIGwpYe5aW8YjqxBtWSIvCS'
-CONSUMER_KEY = "8v37VAM8vNdTPnryGHknPYxPB"
-CONSUMER_SECRET = "VDBqLpbCmNMgJlfccIhESW3SQV65gTozXptLdEOswMWPz2RR1P"
-ACCESS_TOKEN = "1622677068256272385-gclBcFkJPllp0MS8lRiB6k7vlXNTNR"
-ACCESS_TOKEN_SECRET = "bGsN6ZyuxvNqHNNzshOFbsjNB4E6PY6zyXUWCeQHlawLa"
+BEARER_TOKEN = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
 
 # You can authenticate as your app with just your bearer token
 
@@ -22,13 +21,11 @@ client = tweepy.Client(
 )
 
 public_tweets = client.search_recent_tweets(query = "AAPL", tweet_fields=['created_at'], max_results=max_results)
-print(type(public_tweets))
 tweets_list = public_tweets[0]
-print(type(tweets_list))
 for tweet in tweets_list:
-    print(tweet.text)
-    print(tweet.created_at)
-    print("-----------------------------------------------------------------------------")
-    
-#with open("foo.txt", "a") as f:
- #    f.write("new line\n")
+    with open('tweets.txt', 'a') as f:
+        f.write(tweet.text + '\n')
+        f.write(str(tweet.created_at) + '\n')
+        f.write("========-========-========-========-========-========-========-========-\n")
+        f.close()
+        
