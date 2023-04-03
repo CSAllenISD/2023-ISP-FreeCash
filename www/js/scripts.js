500fb49c4caca91184658251ac81a122cf187374
Boden-C <70297682+Boden-C@users.noreply.github.com>
2023-04-03 12:29:33 -0500

    Added array of stocks for local storage of stock

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,3 +1,19 @@
+const arrayOfStocks = (function() {
+    var stocks = [];
+  
+    // Temporary add stock
+    const apple = new Stock('Apple Inc.', 'AAPL', 'Strong Buy', '+10%');
+    stocks.push(apple);
+    const google = new Stock('Alphabet Inc.', 'GOOGL', 'Buy', '+10%');
+    stocks.push(google);
+    const microsoft = new Stock('Microsoft Corporation', 'MSFT', 'Strong Buy', '+10%');
+    stocks.push(microsoft);
+    const amazon = new Stock('Amazon.com Inc.', 'AMZN', 'Strong Buy', '+10%');
+    stocks.push(amazon);
+  
+    return stocks;
+  })();
+
 window.addEventListener("DOMContentLoaded", (event) => {
 
     // Collapse responsive navbar when toggler is visible
