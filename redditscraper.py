4be9b6f8a15ebdb8d8febc1fef07a754feb2f2c1
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2023-04-14 23:10:31 -0500

    Update redditscraper.py

--- redditscraper.py
+++ redditscraper.py
@@ -1,14 +1,37 @@
+import configparser
 import praw
+import time
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+client_id = config['reddit']['client_id']
+client_secret = config['reddit']['client_secret']
+user_agent = config['reddit']['user_agent']
 
 reddit = praw.Reddit(
-    client_id="your_client_id",
-    client_secret="your_client_secret",
-    user_agent="your_user_agent",
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent
 )
 
 subreddit = reddit.subreddit("wallstreetbets")
+results = []
 
-for submission in subreddit.search("$AAPL", sort="new", limit=10):
-    print(submission.title)
-    print(submission.url)
+try:
+    for submission in subreddit.search("$AAPL", sort="new", limit=10):
+        result = {
+            'title': submission.title,
+            'url': submission.url
+        }
+        results.append(result)
+        
+except praw.exceptions.APIException as e:
+    print(f"Encountered an error: {e}")
+    print("Waiting for 60 seconds before retrying...")
+    time.sleep(60)
+
+for result in results:
+    print(result['title'])
+    print(result['url'])
     print("--------------------")
