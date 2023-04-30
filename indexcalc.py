e2c293a678680a6ae00eb6200c7a788b3fc56a23
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2023-04-30 12:01:53 -0500

    Update indexcalc.py

--- indexcalc.py
+++ indexcalc.py
@@ -2,20 +2,22 @@ import json
 from textblob import TextBlob
 
 def find_most_positive_sentiment(tweet_json):
-    # Load the tweet data from the JSON string
-    tweet_data = json.loads(tweet_json)
-    
-    # Create a list to store the sentiment polarity scores
-    sentiment_scores = []
-    
-    # Loop through each tweet and get the sentiment polarity score
-    for tweet in tweet_data:
-        text = tweet["text"]
-        sentiment = TextBlob(text).sentiment.polarity
-        sentiment_scores.append(sentiment)
-    
-    # Get the index of the maximum value in the list of sentiment scores
-    max_index = sentiment_scores.index(max(sentiment_scores))
-    
-    # Return the index
-    return max_index
+    try:
+        # Load the tweet data from the JSON string
+        tweet_data = json.loads(tweet_json)
+
+        # Create a list to store the sentiment polarity scores
+        sentiment_scores = []
+
+        # Loop through each tweet and get the sentiment polarity score using list comprehension
+        sentiment_scores = [TextBlob(tweet["text"]).sentiment.polarity for tweet in tweet_data]
+
+        # Get the index of the maximum value in the list of sentiment scores
+        max_index = sentiment_scores.index(max(sentiment_scores))
+
+        # Return the index
+        return max_index
+
+    except json.JSONDecodeError as e:
+        print(f"Error: {e}")
+        return None
